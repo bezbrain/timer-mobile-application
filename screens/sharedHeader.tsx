9eb4f5279aeea95ddfa70bcc5f3a5d3b9080df3b
bezbrain <belloquadriolawale@gmail.com>
2024-04-15 09:17:54 +0100

    Use expo icons in the shared header component

--- screens/sharedHeader.tsx
+++ screens/sharedHeader.tsx
@@ -1,12 +1,34 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { MaterialIcons, Octicons } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
+import { StyleSheet, Text, View } from "react-native";
+import { Colors } from "../utils/colors";
+
+const { whitish } = Colors;
 
 const SharedHeader = () => {
   return (
-    <View>
-      <Text>Shared Header</Text>
+    <View style={styles.sharedRoot}>
+      <MaterialIcons name="access-alarm" size={24} color={whitish} />
+      <Ionicons name="time-outline" size={24} color={whitish} />
+      <MaterialCommunityIcons
+        name="timer-sand-complete"
+        size={24}
+        color={whitish}
+      />
+      <Octicons name="stopwatch" size={24} color={whitish} />
+      <Entypo name="dots-three-vertical" size={24} color={whitish} />
     </View>
   );
 };
 
 export default SharedHeader;
+
+const styles = StyleSheet.create({
+  sharedRoot: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+});
--- utils/colors.ts
+++ utils/colors.ts
@@ -1,4 +1,5 @@
 export const Colors = {
   primary600: "#141a1c",
   accent400: "#2e788c",
+  whitish: "white",
 };
