6dee5262a4f8b98a87e48a00d443babfb0cb7289
bezbrain <belloquadriolawale@gmail.com>
2024-04-16 13:21:39 +0100

    Write the logic for stop watch start and stop

--- context/currentAlarm.context.tsx
+++ context/currentAlarm.context.tsx
@@ -5,8 +5,13 @@ const AppContext = createContext<any | undefined>(undefined);
 
 export const CurrentTimeProvider = ({ children }: ChildrenType) => {
   const [isAlarmSet, setIsAlarmSet] = useState<boolean>(false);
+
   const [countDown, setCountDown] = useState<number>(5);
   const [startCountDown, setStartCountDown] = useState<boolean>(false);
+
+  const [stopWatchCount, setStopWatchCount] = useState<number>(0);
+  const [startStopWatchCount, setStartStopWatchCount] =
+    useState<boolean>(false);
 
   return (
     <AppContext.Provider
@@ -17,6 +22,10 @@ export const CurrentTimeProvider = ({ children }: ChildrenType) => {
         setCountDown,
         startCountDown,
         setStartCountDown,
+        stopWatchCount,
+        setStopWatchCount,
+        startStopWatchCount,
+        setStartStopWatchCount,
       }}
     >
       {children}
--- screens/stopWatch.tsx
+++ screens/stopWatch.tsx
@@ -1,12 +1,76 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { ContainerWrappers } from "../components/helpers";
+import { RoundedButton } from "../components/general";
+import { Colors } from "../utils/colors";
+import { useCurrentAlarm } from "../context/currentAlarm.context";
 
 const StopWatch = () => {
+  const {
+    stopWatchCount,
+    setStopWatchCount,
+    startStopWatchCount,
+    setStartStopWatchCount,
+  } = useCurrentAlarm();
+
+  // TRIGGER THE STOPWATCH START OR STOP
+  const handleStopWatch = () => {
+    if (startStopWatchCount) {
+      setStartStopWatchCount(false);
+    } else {
+      setStartStopWatchCount(true);
+    }
+  };
+
+  useEffect(() => {
+    // Initiate Stop watch
+    if (startStopWatchCount) {
+      setTimeout(() => {
+        setStopWatchCount(stopWatchCount + 1);
+      }, 1000);
+    }
+  }, [stopWatchCount, startStopWatchCount]);
+
   return (
-    <View>
-      <Text>Stop Watch</Text>
-    </View>
+    <ContainerWrappers>
+      <View style={styles.stopWatchContainer}>
+        <View style={styles.stopWatchCircle}>
+          <Text style={styles.stopWatchCount}>{stopWatchCount}</Text>
+        </View>
+      </View>
+
+      <RoundedButton handlePress={handleStopWatch}>
+        {!startStopWatchCount && (
+          <Feather name="play" size={24} color={Colors.whitish} />
+        )}
+        {startStopWatchCount && (
+          <Feather name="pause" size={24} color={Colors.whitish} />
+        )}
+      </RoundedButton>
+    </ContainerWrappers>
   );
 };
 
 export default StopWatch;
+
+const styles = StyleSheet.create({
+  stopWatchContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  stopWatchCircle: {
+    height: 250,
+    width: 250,
+    borderRadius: 125,
+    borderWidth: 3,
+    borderColor: Colors.whitish700,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  stopWatchCount: {
+    fontFamily: "degular-bold",
+    color: Colors.whitish,
+    fontSize: 48,
+  },
+});
